63f8429751b338cd8a58feac5d430b86c7354379
cliffgla <cliffordgagliardo@gmail.com>
2017-10-20 20:52:33 +0100

    converted value into val to map to -10 to +10 scale

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var value = 0
 function changeBright() {
     value = $("#controls input").val();
+    val = parseInt(value);
+    console.log(value);
+
+    val = (val+10)/10.0;
 
     $("p").html("<p>"+ value + "</p>");
-    $("#photo img").css("filter", "brightness("+ value +")");
+    $("#photo img").css("filter", "brightness("+ val +")");
 
 
 
