2caa39ed4317a68f753f15e194ea45c12b3c1fc1
cliffgla <cliffordgagliardo@gmail.com>
2017-10-20 20:02:43 +0100

    added jquery to change css filter: brightness value

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var value = 0
 function changeBright() {
     value = $("#controls input").val();
     $("p").html("<p>"+ value + "</p>");
+    $("#photo img").css("filter", "brightness("+ value +")")
+
+
+
 }
-
-
 
 $("input[type=range]").change(changeBright);
