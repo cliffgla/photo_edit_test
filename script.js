0a26aff7a0c436e94870986bd2090de16327f763
cliffgla <cliffordgagliardo@gmail.com>
2017-10-22 18:02:28 +0100

    more than one slider causing action, but now lost values showing in <p>

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 var value = 0
 function changeBright() {
-    value = $("input").val();
+    value = $("#brightness-input").val();
     val = parseInt(value);
     console.log(value);
 
     val = (val+10)/10.0;
 
-    $("#brightness-value").html("<p>"+ value + "</p>");
+    $("#brightness-input").html("<p>"+ value + "</p>");
     $("#photo img").css("filter", "brightness("+ val +")");
 
 
 
 }
 
-$("input[type=range]").change(changeBright);
+function changeContrast() {
+    value = $("#contrast-input").val();
+    val = parseInt(value);
+    console.log(value);
+
+    val = (val+10)/10.0;
+
+    $("#contrast-value").html("<p>"+ value + "</p>");
+    $("#photo img").css("filter", "contrast("+ val +")");
+
+
+
+}
+$("#brightness-input[value]").change(changeBright);
+$("#contrast-input[value]").change(changeContrast);
